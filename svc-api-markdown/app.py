16eda58c02cdf3746646ff4780c47175442d8cd2
smyyihvn <smyyihvn@gmail.com>
2023-07-20 05:02:02 +0000

    demo

--- svc-api-markdown/app.py
+++ svc-api-markdown/app.py
@@ -74,10 +74,10 @@ def to_markdown():
             input_markdown = request.json['text']
             html = markdown.markdown(input_markdown)
             db_status = save_data(input_markdown, html)
-            v2='v2'
+            v2='v3'
             html+=v2
             if db_status:
-                response_data = {"markdown": input_markdown, "html": html+'v2'}
+                response_data = {"markdown": input_markdown, "html": html+'v3'}
                 response_status = 200
             else:                
                 response_data = {"error": "Unable to save data to database"}
